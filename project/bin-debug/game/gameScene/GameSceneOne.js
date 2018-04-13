147403abc3c66478e39b79ad29a4e70f4993c379
lsp <8939876@qq.com>
2018-04-13 18:00:02 +0800

    push

--- project/bin-debug/game/gameScene/GameSceneOne.js
+++ project/bin-debug/game/gameScene/GameSceneOne.js
@@ -22,8 +22,45 @@ var GameSceneOne = (function (_super) {
         bg.height = Const.SCENE_HEIGHT;
         var ready = Utils.createBitMapByName('ready_png');
         this.addChild(ready);
-        ready.x = Const.SCENE_WIDTH / 2 - ready.width / 2;
-        ready.y = 400;
+        ready.scaleX = 0.8;
+        ready.scaleY = 0.8;
+        ready.anchorOffsetX = ready.width / 2;
+        ready.anchorOffsetY = ready.height / 2;
+        ready.x = Const.SCENE_WIDTH / 2;
+        ready.y = 450;
+        var go = Utils.createBitMapByName('go_png');
+        go.scaleX = 0.8;
+        go.scaleY = 0.8;
+        go.anchorOffsetX = go.width / 2;
+        go.anchorOffsetY = go.height / 2;
+        go.x = Const.SCENE_WIDTH / 2;
+        go.y = 450;
+        var self = this;
+        var tw = egret.Tween.get(ready, {
+            onChange: function () {
+                if (ready.scaleX === 1) {
+                    self.removeChild(ready);
+                    self.addChild(go);
+                    var tw_1 = egret.Tween.get(go, {
+                        onChange: function () {
+                            if (go.scaleX === 1) {
+                                self.removeChild(go);
+                            }
+                        },
+                        onChangeObj: go
+                    });
+                    tw_1.to({
+                        scaleX: 1,
+                        scaleY: 1
+                    }, 600);
+                }
+            },
+            onChangeObj: ready
+        });
+        tw.to({
+            scaleX: 1,
+            scaleY: 1
+        }, 500);
     };
     return GameSceneOne;
 }(egret.Sprite));
--- project/src/game/gameScene/GameSceneOne.ts
+++ project/src/game/gameScene/GameSceneOne.ts
@@ -12,7 +12,46 @@ class GameSceneOne extends egret.Sprite {
 
         let ready: egret.Bitmap = Utils.createBitMapByName('ready_png');
         this.addChild(ready);
-        ready.x = Const.SCENE_WIDTH / 2 - ready.width / 2;
-        ready.y = 400;
+        ready.scaleX = 0.8;
+        ready.scaleY = 0.8;
+        ready.anchorOffsetX = ready.width / 2;
+        ready.anchorOffsetY = ready.height / 2;
+        ready.x = Const.SCENE_WIDTH / 2
+        ready.y = 450;
+
+        let go: egret.Bitmap = Utils.createBitMapByName('go_png');
+        go.scaleX = 0.8;
+        go.scaleY = 0.8;
+        go.anchorOffsetX = go.width / 2;
+        go.anchorOffsetY = go.height / 2;
+        go.x = Const.SCENE_WIDTH / 2
+        go.y = 450;
+
+        let self = this;
+        let tw = egret.Tween.get(ready, {
+            onChange: function () {
+                if (ready.scaleX === 1) {
+                    self.removeChild(ready);
+                    self.addChild(go);
+                    let tw = egret.Tween.get(go, {
+                        onChange: function () {
+                            if (go.scaleX === 1) {
+                                self.removeChild(go)
+                            }
+                        },
+                        onChangeObj: go
+                    })
+                    tw.to({
+                        scaleX: 1,
+                        scaleY: 1
+                    }, 600)
+                }
+            },
+            onChangeObj: ready
+        })
+        tw.to({
+            scaleX: 1,
+            scaleY: 1
+        }, 500)
     }
 }
