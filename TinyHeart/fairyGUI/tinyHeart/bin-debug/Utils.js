5f12eb3e0121c67ce9a24f8e0e3e0fcf4ae1f871
zhiyu <zhengzhiyu_@outlook.com>
2018-04-21 12:53:37 +0800

    up

---
+++ TinyHeart/fairyGUI/tinyHeart/bin-debug/Utils.js
@@ -0,0 +1,22 @@
+var __reflect = (this && this.__reflect) || function (p, c, t) {
+    p.__class__ = c, t ? t.push(c) : t = [c], p.__types__ = p.__types__ ? t.concat(p.__types__) : t;
+};
+var Utils = (function () {
+    function Utils() {
+    }
+    Utils.enterScene = function (sceneName) {
+        var numChildren = fairygui.GRoot.inst.nativeStage.numChildren;
+        fairygui.GRoot.inst.removeChildren(0, numChildren, true);
+        if (sceneName['getView']) {
+            fairygui.GRoot.inst.addChild(sceneName['getView']());
+        }
+    };
+    Utils.createBitmapByName = function (name) {
+        var result = new egret.Bitmap();
+        var texture = RES.getRes(name);
+        result.texture = texture;
+        return result;
+    };
+    return Utils;
+}());
+__reflect(Utils.prototype, "Utils");
